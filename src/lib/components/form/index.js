7bbc78bf3fb161ac1c5a4c7ee66a403d45fc7a0f
Schmell <schmell_ya_later@hotmail.com>
2023-08-26 13:04:28 -0400

    c

--- src/lib/components/form/index.js
+++ src/lib/components/form/index.js
@@ -1,8 +1,8 @@
 export { default as Form } from './form.svelte';
 export { default as Textarea } from './Textarea.svelte';
-export { default as Button } from './button.svelte';
 export { default as Select } from './select.svelte';
 export { default as Input } from './input.svelte';
 export { default as File } from './file.svelte';
 export { default as Check } from './check.svelte';
 export { default as Hidden } from './hidden.svelte';
+export { default as Button } from './button.svelte';
