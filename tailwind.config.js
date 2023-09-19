bdac523e8e635314b537ef1d7974b2c7d2b4f0e7
Schmell <schmell_ya_later@hotmail.com>
2023-09-19 10:58:05 -0400

    I duunnnooo

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config}*/
+
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+
+	daisyui: {
+		themes: ['light', 'dark', 'cupcake', 'valentine', 'halloween', 'bumblebee']
+		// themes: themes
+	}
+}
+
+module.exports = config
